fe8f8ced545df54bb046c31a1f3cdfec33d27984
wrong5210 <36704335+wrong5210@users.noreply.github.com>
2021-03-07 14:53:06 +0800

    Update app1.js

--- app1.js
+++ app1.js
@@ -15,27 +15,6 @@ const TG_BOT_TOKEN = process.env.TG_BOT_TOKEN;
 const TG_USER_ID = process.env.TG_USER_ID;
 
 
-// =======================================telegram机器人通知设置区域===========================================
-//此处填你telegram bot 的Token，例如：1077xxx4424:AAFjv0FcqxxxxxxgEMGfi22B4yh15R5uw
-//(环境变量名 TG_BOT_TOKEN)
-let TG_BOT_TOKEN = '';
-//此处填你接收通知消息的telegram用户的id，例如：129xxx206
-//(环境变量名 TG_USER_ID)
-let TG_USER_ID = '';
-
-if (process.env.TG_BOT_TOKEN) {
-  TG_BOT_TOKEN = process.env.TG_BOT_TOKEN;
-}
-if (process.env.TG_USER_ID) {
-  TG_USER_ID = process.env.TG_USER_ID;
-}
-
-
-
-
-
-
-
 async function downFile () {
     // const url = 'https://cdn.jsdelivr.net/gh/NobyDa/Script@master/JD-DailyBonus/JD_DailyBonus.js'
     const url = 'https://raw.githubusercontent.com/NobyDa/Script/master/JD-DailyBonus/JD_DailyBonus.js';
@@ -80,57 +59,20 @@ async function start() {
   await exec("node JD_DailyBonus.js >> result.txt");
   console.log('执行完毕')
 
-  if (TG_BOT_TOKEN && TG_USER_ID) {
-      const options = {
-        url: `https://api.telegram.org/bot${TG_BOT_TOKEN}/sendMessage`,
-        body: `chat_id=${TG_USER_ID}&text=${text}\n${desp}&disable_web_page_preview=true`,
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }
-      }
-      if (process.env.TG_PROXY_HOST && process.env.TG_PROXY_PORT) {
-        const tunnel = require("tunnel");
-        const agent = {
-          https: tunnel.httpsOverHttp({
-            proxy: {
-              host: process.env.TG_PROXY_HOST,
-              port: process.env.TG_PROXY_PORT * 1
-            }
-          })
-        }
-        Object.assign(options, { agent })
-      }
-      $.post(options, (err, resp, data) => {
-        try {
-          if (err) {
-            console.log('telegram发送通知消息失败！！\n')
-            console.log(err);
-          } else {
-            data = JSON.parse(data);
-            if (data.ok) {
-              console.log('Telegram发送通知消息完成。\n')
-            } else if (data.error_code === 400) {
-              console.log('请主动给bot发送一条消息并检查接收用户ID是否正确。\n')
-            } else if (data.error_code === 401){
-              console.log('Telegram bot token 填写错误。\n')
-            }
-          }
-        } catch (e) {
-          $.logErr(e, resp);
-        } finally {
-          resolve(data);
-        }
-      })
-    } else {
-      console.log('您未提供telegram机器人推送所需的TG_BOT_TOKEN和TG_USER_ID，取消telegram推送消息通知\n');
-      resolve()
-    }
-    
-    
-    
-    
+ 
     
     if (serverJ) {
+    const path = "./result.txt";
+    let content = "";
+    if (fs.existsSync(path)) {
+      content = fs.readFileSync(path, "utf8");
+    }
+    let t = content.match(/【签到概览】:((.|\n)*)【签到奖励】/)
+    let res = t ? t[1].replace(/\n/,'') : '失败'
+    let t2 = content.match(/【签到奖励】:((.|\n)*)【其他奖励】/)
+    let res2 = t2 ? t2[1].replace(/\n/,'') : '总计0'
+    
+    if (TG_BOT_TOKEN && TG_USER_ID) {
     const path = "./result.txt";
     let content = "";
     if (fs.existsSync(path)) {
